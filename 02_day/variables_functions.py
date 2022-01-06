80a232fe0424c04dd5e0dde4cb891ae93d40a00f
Javier-VL <javiervelazquezlozano@gmail.com>
2022-01-06 02:31:21 -0600

    dia 2

---
+++ 02_day/variables_functions.py
@@ -0,0 +1,17 @@
+#Exercise level 1
+
+first_name = "Javi"
+last_name="velazquez"
+
+is_marriesd ="desafortunadamente si"
+is_light_on ="No"
+
+alpha, beta, gama = "soy ", "el", 666
+
+#Exercise level 2
+print(type(gama))
+
+print(len(last_name))
+
+print(f"los nombres son simetros.. {(len(last_name)==len(first_name))}")
+help("lambda")
